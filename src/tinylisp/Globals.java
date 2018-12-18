b84ff0c382bf7cb771e43f0a180d9d0a7349b24b
Abraham <abrahampost@yahoo.com>
2018-12-18 01:58:01 -0500

    Added '*', '/' and '-' support

--- src/tinylisp/Globals.java
+++ src/tinylisp/Globals.java
@@ -25,6 +25,70 @@ public class Globals {
 			}
 			
 		});
+		env.define(new Token("-"), new Callable() {
+
+			@Override
+			public Object call(Interpreter interpreter, List<Object> arguments, int line) {
+				int start = (int) arguments.get(0);
+				for (int i = 1; i < arguments.size(); i++) {
+					Object arg = arguments.get(i);
+					if (!(arg instanceof Integer)) {
+						throw new RuntimeError(line, "expected int, received " + arg.getClass().getSimpleName());
+					}
+					start -= (Integer)arg;
+				}
+				return start;
+			}
+
+			@Override
+			public int arity() {
+				return 1;
+			}
+			
+		});
+		env.define(new Token("*"), new Callable() {
+
+			@Override
+			public Object call(Interpreter interpreter, List<Object> arguments, int line) {
+				int total = (int) arguments.get(0);
+				for (int i = 1; i < arguments.size(); i++) {
+					Object arg = arguments.get(i);
+					if (!(arg instanceof Integer)) {
+						throw new RuntimeError(line, "expected int, received " + arg.getClass().getSimpleName());
+					}
+					total *= (Integer)arg;
+				}
+				return total;
+			}
+
+			@Override
+			public int arity() {
+				return 1;
+			}
+			
+		});
+		
+		env.define(new Token("/"), new Callable() {
+
+			@Override
+			public Object call(Interpreter interpreter, List<Object> arguments, int line) {
+				int total = (int) arguments.get(0);
+				for (int i = 1; i < arguments.size(); i++) {
+					Object arg = arguments.get(i);
+					if (!(arg instanceof Integer)) {
+						throw new RuntimeError(line, "expected int, received " + arg.getClass().getSimpleName());
+					}
+					total /= (Integer)arg;
+				}
+				return total;
+			}
+
+			@Override
+			public int arity() {
+				return 1;
+			}
+			
+		});
 		return env;
 	}
 }
